596ccd950d7a1679e0287d57467a88e17ec34a6e
maxx-maxxing <maxxlazos93@gmail.com>
2025-12-05 08:12:21 -0700

    Finished playercontroller. Moving on to level design.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,45 @@
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rig;
+    public float jumpForce;
+    public SpriteRenderer sr;
+    
+    private bool isGrounded;
 
     private void FixedUpdate()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
         rig.linearVelocity = new Vector2(moveInput * moveSpeed, rig.linearVelocity.y);
+
+        if (rig.linearVelocity.x > 0)
+        {
+            sr.flipX = true;
+        }
+        else if (rig.linearVelocity.x < 0)
+        {
+            sr.flipX = false;
+        } 
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            isGrounded = false;
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.GetContact(0).normal == Vector2.up)
+        {
+            isGrounded = true;
+        }
+    }
+
+    
 }
