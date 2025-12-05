49e601e6d49cadb6b87033269724b62006d883a5
maxx-maxxing <maxxlazos93@gmail.com>
2025-12-05 07:08:33 -0700

    Added horizontal movement to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,14 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+    public Rigidbody2D rig;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rig.linearVelocity = new Vector2(moveInput * moveSpeed, rig.linearVelocity.y);
     }
 }
