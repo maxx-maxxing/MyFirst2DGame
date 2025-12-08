80ef59d4516c02f076a1c1fd7a62f6c86772f164
maxx-maxxing <maxxlazos93@gmail.com>
2025-12-08 08:37:35 -0700

    Set up enemy sprite, collider, movement script. Next moving to game over

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector3 moveOffset;
+
+    private Vector3 startPos;
+    private Vector3 targetPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+        targetPos = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f)
+        {
+            if(targetPos == startPos)
+            {
+                targetPos = startPos + moveOffset;
+            }
+            else
+            {
+                targetPos = startPos;
+            }
+        }
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 from;
+        Vector3 to;
+        if (Application.isPlaying)
+        {
+            from = startPos;
+        }
+        else
+        {
+            from = transform.position;
+        }
+        to = from + moveOffset;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawWireSphere(to, 0.2f);
+        Gizmos.DrawWireSphere(from, 0.2f);
+    }
+}
